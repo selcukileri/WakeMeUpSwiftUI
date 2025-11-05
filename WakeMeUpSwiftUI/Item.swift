8e5a44bdeebad8aed6ac7af6111f0390cea060eb
Selçuk İleri <selcukileri98@gmail.com>
2025-11-05 12:04:52 +0300

    Initial Commit

---
+++ WakeMeUpSwiftUI/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WakeMeUpSwiftUI
+//
+//  Created by Selçuk İleri on 5.11.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
